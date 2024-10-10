1b249cdca62e261e80953f26342f4fabe4678a7f
Jatin.Chawla <jatin.chawla@thoughts2binary.com>
2024-10-11 00:40:27 +0530

    user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,61 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler((req, res) => {
-  res.status(200).json({
-    message: "ok",
-    success: true,
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, fullName, password } = req.body;
+  if (
+    [username, email, fullName, password].some((field) => field?.trim === "")
+  ) {
+    throw new ApiError(400, `${field} should not be empty!`);
+  }
+
+  const existingUser = await User.findOne({
+    $or: [{ email, username }],
   });
+
+  if (existingUser) {
+    throw new ApiError(409, "User with this email or username already exist");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (coverImageLocalPath) {
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  }
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
+
+  const user = await User.create({
+    email,
+    fullName,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user!");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully!"));
 });
 
 export { registerUser };
